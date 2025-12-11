c60bec1529a5c5ccecd9c586a1151bc3dbbffa7d
Kratik-Sen <kratiksen064@gmail.com>
2025-12-12 00:08:27 +0530

    update url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://openshelf-frontend.vercel.app",
     credentials: true,
   })
 );
